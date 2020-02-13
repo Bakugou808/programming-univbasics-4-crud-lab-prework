cc517c4a170acccab6a01e79544ba6e17ade9eaa
Janu Sung <janu.m.sung@gmail.com>
2020-02-13 03:40:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  pets = ["cat", "dog", "fish"]
+  pets = ["cat", "dog", "fish", "hamster"]
 end
 
 def add_element_to_end_of_array(array, element)
